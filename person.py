14d9bb1247dddf5a3ec0310f095d246d808e7b3d
Christie Partington <christiedpartington@gmail.com>
2019-12-05 15:15:50 -0700

    Add initialization

--- person.py
+++ person.py
@@ -1,27 +1,49 @@
 class Person:
-    def __init__(self, mother=None, father=None):
+    def __init__(self):
         # Basic information
-        self.age: int
-        self.sex: int
-        self.name: str
-        self.mother = mother
-        self.father = father
+        self.age: int = -1
+        self.sex: int = -1
+        self.name: str = ''
 
-        # Personality information
-        self.rebellious: int
-        self.ambition: int
-        self.hardworking: int
-        self.life_goal: int
+        # First order information
+        self.rebellious: int = -1
+        self.ambition: int = -1
+        self.hardworking: int = -1
+        self.life_goal: int = -1
+        self.lifestyle: int = -1
 
-        # Complex information
-        self.education: int
-        self.income: int
-        self.lifestyle: int
-        self.parenting: int
+        # Second order information
+        self.education: int = -1
+        self.income: int = -1
+        self.parenting_desire: int = -1
+        self.parenting: int = -1
+        self.longevity: int = -1
 
-        # Automated creation
-        if mother is not None and father is not None:
-            self.init_self()
+    def init_self(self, util, mother, father):
+        self.age = util.random_adult_age()
+        self.sex = util.random_sex()
+        self.name = util.random_name(self.sex)
 
-    def init_self(self):
-        pass
+        self.rebellious = util.random_attribute()
+        self.ambition = util.child_attribute(mother.ambition, father.ambition)
+        self.hardworking = util.child_attribute(mother.hardworking, father.hardworking, self.rebellious)
+        self.life_goal = util.child_life_goal(mother.life_goal, father.life_goal, self.rebellious)
+        self.lifestyle = util.child_attribute(mother.lifestyle, father.lifestyle, self.rebellious)
+
+        self.education = util.child_education(mother, father, self)
+        self.income = util.child_income(self)
+        self.parenting_desire = util.child_parenting_desire(mother, father, self)
+        self.parenting = util.child_parenting(mother, father, self)
+        self.longevity = util.child_longevity(self)
+
+    def get_quality(self):
+        q = self.ambition + self.hardworking + self.education + self.lifestyle + self.parenting
+        return q
+
+    def __str__(self):
+        return '{}: Age: {} Sex: {} Longevity: {}y\n' \
+               '   Rebellion: {} Ambition: {} Hardworking: {}\n' \
+               '   Life goal: {} Lifestyle: {} Education: {}\n' \
+               '   Income: {} Parenting Desire: {} Quality: {}\n'\
+            .format(self.name, self.age, self.sex, self.longevity, self.rebellious, self.ambition, self.hardworking,
+                    self.life_goal, self.lifestyle, self.education, self.income, self.parenting_desire, self.parenting)
